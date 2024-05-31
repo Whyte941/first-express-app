5d2b3286d28e042d724c71b3a41d980948d847c2
Dammitech1 <muhammed.m2302994@st.futminna.edu.ng>
2024-05-31 07:18:05 -0700

    added to my first web server

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express")
 const app = express()
-const port = 3000
+const port = 4000
 
 app.get("/", (req, res) => {
-    res.send("hello world! how are you today?")
+    res.send("hello world! i am here today")
+})
+app.get("/login", (req, res) => {
+    res.send("do you want to login to your app?")
+})
+app.get("/register", (req, res) => {
+    res.send("do you want to register an account?")
+})
+app.get("/register/nigeria", (req, res) => {
+    res.send("do you want to register an account?")
 })
 
 app.listen(port, () => {
